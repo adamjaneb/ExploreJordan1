ab2e959b640268fb4a16e47561e2f428a0d20803
Adam Janeb <adamjaneb7@gmail.com>
2024-09-29 21:22:34 +0300

    Add about us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: BodyComponent },
   { path: 'contact', component: ContactUsComponent },
-  { path: 'home', component:BodyComponent}
+  { path: 'home', component:BodyComponent},
+  {path: 'about-us', component:AboutUsComponent}
 ]

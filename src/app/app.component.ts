5345cdb8cc7ba908121752350114712eb9534ca5
Adam Janeb <adamjaneb7@gmail.com>
2024-09-29 21:03:27 +0300

    Add contact us

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,NavbarComponent,BodyComponent,FooterComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    HeaderComponent,
+    NavbarComponent,
+    BodyComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
-}
+export class AppComponent { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BodyComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'home', component:BodyComponent}
+]
